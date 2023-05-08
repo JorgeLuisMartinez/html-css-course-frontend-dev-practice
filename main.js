99ff8beea73970b6f3670703856ab4b41c4c747f
JorgeLuisMartinez <Jorge389.jm@gmail.com>
2023-05-08 17:19:21 -0500

    Lista de productos:HTML a partir de arrays

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenuView = document.querySelector('.desktop-menu');
 const mobileMenuView = document.querySelector('.mobile-menu');
 const burguerMenuBtn = document.querySelector('.menu');
-const shopingCartView = document.querySelector('.product-detail')
-const shopingCartBtn = document.querySelector('.navbar-shoping-cart')
+const shopingCartView = document.querySelector('.product-detail');
+const shopingCartBtn = document.querySelector('.navbar-shoping-cart');
+// array de productos
+const cardsContainer = document.querySelector('.cards-container');
+const productList = [];
+
 
 shopingCartBtn.addEventListener('click', ()=>{
     const isMobileMenuViewClosed = mobileMenuView.classList.contains('inactive');
@@ -31,3 +35,90 @@ menuEmail.addEventListener('click', ()=>{
     desktopMenuView.classList.toggle('inactive')
 })
 
+// Creando las products cards
+productList.push({
+    name : 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name : 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name : 'Pc',
+    price: 320,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name : 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name : 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name : 'Pc',
+    price: 320,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name : 'Pc',
+    price: 320,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name : 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name : 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name : 'Pc',
+    price: 320,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+function renderProducts(arr){
+    for (product of arr) {
+        // En este primer bloque de codigo creamos la estructura de una product cart pero dinamica
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const img = document.createElement('img');
+        img.setAttribute('src', product.image)
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        // En este codigo de aca con append damos orden a los elementos, que elementos van dentro de que otros
+        
+        productInfoFigure.appendChild(productImgCard);
+        productInfoDiv.append(productPrice, productName);
+        productInfo.append(productInfoDiv,productInfoFigure)
+        productCard.append(img,productInfo);
+        cardsContainer.append(productCard);
+    }
+}
+
+renderProducts(productList)
